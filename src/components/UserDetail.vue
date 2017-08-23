aea93330abf12152a3a5164212ed2c82ed6934ee
Emilie G <adamanska@gmail.com>
2017-08-23 11:51:27 +0200

    difference between "name" and "myName"

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,14 +3,14 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>
-            User Name : {{ name }}
+            User Name : {{ myName }}
         </p>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: ['myName']
         // on indique ici que l'on veut accéder à name dans le parent User.vue - est-ce un parent?
     }
 </script>
